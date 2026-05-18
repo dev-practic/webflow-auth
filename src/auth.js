9ad6ea8c30aaed7ab560a3cb16f13d210df5c787
Mikhail <mm@MacBook-Air-Mikhail.local>
2026-05-18 10:15:50 -0700

    Add credentials check

--- src/auth.js
+++ src/auth.js
@@ -2,5 +2,7 @@
 function login(username, password) {
     // TODO: Implement
 }
-
+function checkCredentals(username, password) {  // опечатка в названии
+    return username && password;
+}
 module.exports = { login };
