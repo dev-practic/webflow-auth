6fecbccedb137026c6d95041513ae06fd07f4aff
Mikhail <mm@MacBook-Air-Mikhail.local>
2026-05-18 10:21:09 -0700

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -4,5 +4,7 @@ function validateEmail(email) {
 }
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
+function validatePassword(password) {
+    return password.length >= 8;
 }
 module.exports = { validateEmail };
